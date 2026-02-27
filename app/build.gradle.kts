669102eb3f8bf1d6cabd97a641222ace9fa118cf
Developer <dev@mimascota.app>
2026-02-27 15:32:29 -0300

    chore: guardar estado actual para continuar luego

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.christianstuart.mimascota2"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
